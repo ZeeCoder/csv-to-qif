1ff582edc13bc84c2d6dbcf136e8855cafb15e41
Viktor Hubert <rpgmorpheus@gmail.com>
2019-11-02 16:48:59 +0100

    Make the cash converter work specifically with HUF

--- converters/cash.js
+++ converters/cash.js
@@ -1,10 +1,11 @@
+// Note: works with Hungarian Forints. (Ex: "Ft12,000.00")
 const moment = require("moment");
 
 const delimiter = ",";
 
 const convert = rowData => ({
   date: moment(rowData["Dátum"], "YYYY-MM-DD").format("DD/MM/YYYY"),
-  amount: parseFloat(rowData["Kiadás"].replace("Ft", "")),
+  amount: parseFloat(rowData["Kiadás"].replace("Ft", "").replace(",", "")),
   payee: " ",
   memo: rowData["Megjegyzés"],
   category: " "
